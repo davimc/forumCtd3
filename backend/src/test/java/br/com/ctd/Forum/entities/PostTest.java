e2f0b946dc4f8f2884244e91a1a067dc95f407fd
davimc <davimatosc@hotmail.com>
2023-02-24 07:53:19 -0300

    Unit test

--- backend/src/test/java/br/com/ctd/Forum/entities/PostTest.java
+++ backend/src/test/java/br/com/ctd/Forum/entities/PostTest.java
@@ -1,19 +1,34 @@
 package br.com.ctd.Forum.entities;
 
+import br.com.ctd.Forum.entities.enums.PostMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PostTest {
 
     private SamplePost post;
 
     @Test
-    public void postShouldInstantiateWhenConcretClass() throws ClassNotFoundException {
-        Class<SamplePost> classe = SamplePost.class;
-        System.out.println(classe.getPermittedSubclasses().length);
-        Arrays.stream(classe.getPermittedSubclasses()).forEach(p-> System.out.println(p.getSimpleName()));
+    public void postMapperShouldHaveSameNumberOfPermitted() throws ClassNotFoundException {
+        int postMapperrQtt = PostMapper.values().length;
+        List<Class<?>> permitedPost = getPermittedPost();
+        int postPermittedQtt = permitedPost.size();
+
+        Assertions.assertEquals(postMapperrQtt, postPermittedQtt);
+    }
+
+    private List<Class<?>> getPermittedPost() {
+        List<Class<?>> classes =  Arrays.stream(PostCommnad.class.getPermittedSubclasses()).toList();
+        List<Class<?>> permittedClasses = new ArrayList<>();
+        classes.stream().forEach(c -> permittedClasses.addAll(Arrays.stream(c.getPermittedSubclasses()).toList()));
+        String s1 = "java";
+
+        return permittedClasses;
     }
 }
